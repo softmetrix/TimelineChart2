c474ffa58bcdc56655cf3df4616487e840b1c9fa
fenixjz <fenixjz@gmail.com>
2019-05-27 01:10:35 +0200

    Last assets commit from BB

--- vendor/sfmx-assets/js/vendors/sfmx.js
+++ vendor/sfmx-assets/js/vendors/sfmx.js
@@ -1,32 +1,21 @@
 'use strict';
 /**
  * SOFTMETRIX assets JS
+ * Created by SoftMetrix
+ */
+
+/**
+ * Main JS object (literal)
+ *
+ * @type {{}}
  */
 const SFMX = {};
 
 /**
- * Arrow creator
- * EXAMPLE:
- * SFMX.arrow('.arrow-container', '#f30838', 300, 30);
+ * Document get Element shorthand
  *
- * @param ele [element selector]
- * @param c   [color]
- * @param w   [width]
- * @param h   [height]
- */
-SFMX.arrow = function (ele, c, w, h, hideTail, hideHead) {
-    // Sum of tail + head
-    let tailHead = parseInt(10 + 24);
-    // Prevent if arrow size smaller then tail + head
-    SFMX.ele(ele).style.width = w < tailHead ? tailHead + 1 + 'px' : w + 'px';
-    SFMX.ele(ele).style.height = h + 'px';
-    SFMX.ele(ele).innerHTML = SFMX.arrowSvg(c, w, h, hideTail, hideHead);
-};
-
-/**
- *
- * @param ele
- * @returns {element}
+ * @param ele [element class, id...]
+ * @returns mixed [selected element or false]
  */
 SFMX.ele = function (ele) {
     let element = document.querySelector(ele);
@@ -34,33 +23,56 @@ SFMX.ele = function (ele) {
 };
 
 /**
+ * Arrow creator
+ * EXAMPLE:
+ * SFMX.arrow('.arrow-container', '#f30838', 300, 30, true, false);
+ *
+ * @param ele [element selector]
+ * @param c   [color]
+ * @param w   [width]
+ * @param h   [height]
+ * @param hideTail [bool]
+ * @param hideHead [bool]
+ * @return mixed
+ */
+SFMX.arrow = function (ele, c, w, h, hideTail, hideHead) {
+    // Sum of tail + head
+    let tailHead = parseInt(10 + 24);
+    // Prevent if arrow size smaller then tail + head
+    SFMX.ele(ele).style.width = w < tailHead ? tailHead + 1 + 'px' : w + 'px';
+    SFMX.ele(ele).style.height = h + 'px';
+    // noinspection JSUndefinedPropertyAssignment
+    SFMX.ele(ele).innerHTML = SFMX.arrowSvg(c, w, h, hideTail, hideHead);
+};
+
+/**
  * Arrow SVG
  * Tail + Body + Head
- * @param color
- * @param w
- * @param h
- * @returns {string}
+ *
+ * @param color [string]
+ * @param w [int]
+ * @param h [int]
+ * @param hideTail [bool]
+ * @param hideHead [bool]
+ * @returns string [SVG for arrow]
  */
 SFMX.arrowSvg = function (color, w, h, hideTail, hideHead) {
     // Sum of tail + head
     let tailHead = 0;
-    if(!hideTail) {
-        tailHead += 10;
-    }
-    if(!hideHead) {
-        tailHead += 24;
-    }
+    tailHead += !hideTail ? 10 : tailHead;
+    tailHead += !hideHead ? 24 : tailHead;
 
     // Prevent if arrow size smaller then tail + head
     w = w < tailHead ? 1 + 'px' : w - tailHead + 'px';
     h = h + 'px';
 
+    // 3 part of arrow (tail, body and head)
     let tailSvg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="10" height="32" viewBox="0 0 10 32"' +
         ' preserveAspectRatio="none" style="fill: ' + color + ';height: ' + h + '" class="svg-arrow' +
         ' arrow-tail">' +
         '        <path d="M-0.003 1.959l10.314 14.024-10.314 14.059h10.314v-28.083z"></path>' +
         '    </svg>\n';
-    let bodySvg ='<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="28" height="32" viewBox="0 0 28 32" preserveAspectRatio="none" style="fill: ' +
+    let bodySvg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="28" height="32" viewBox="0 0 28 32" preserveAspectRatio="none" style="fill: ' +
         color + ';width: ' + w + ';height: ' + h + '" class="svg-arrow arrow-body">' +
         '        <path d="M0 1.956h28.086v28.086h-28.086v-28.086z"></path>\n' +
         '    </svg>\n';
@@ -70,13 +82,11 @@ SFMX.arrowSvg = function (color, w, h, hideTail, hideHead) {
         '    </svg>';
 
     let resultSvg = '';
-    if(!hideTail) {
-        resultSvg += tailSvg;
-    }
+
+    // Set what include in arrow
+    resultSvg += !hideTail ? tailSvg : '';
     resultSvg += bodySvg;
-    if(!hideHead) {
-        resultSvg += headSvg;
-    }
+    resultSvg += !hideHead ? headSvg : '';
 
     return resultSvg;
 };
